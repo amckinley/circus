0d366ee5749e332e0d688db446bf30eccf961edb
Sebastien Pahl <sebastien@cloudflare.com>
2013-05-21 12:44:26 -0700

    Fix circus variable regex broken in merge

--- circus/util.py
+++ circus/util.py
@@ -434,8 +434,8 @@ def resolve_name(import_name, silent=False):
             raise ImportStringError(import_name, e), None, sys.exc_info()[2]
 
 
-_PATTERN1 = r'\$\(%s\.([\w\._-]+)\)'
-_PATTERN2 = r'\(\(%s\.([\w\._-]+)\)\)'
+_PATTERN1 = r'\$\(%s\.([\w\.\-_]+)\)'
+_PATTERN2 = r'\(\(%s\.([\w\.\-_]+)\)\)'
 _CIRCUS_VAR = re.compile(_PATTERN1 % 'circus' + '|' +
                          _PATTERN2 % 'circus', re.I)
 
@@ -457,7 +457,7 @@ def replace_gnu_args(data, prefix='circus', **options):
             fmt_options[key] = value
 
     if prefix is None:
-        match = re.compile(r'\$\(([\w\._-]+)\)|\(\(([\w\._-]+)\)\)', re.I)
+        match = re.compile(r'\$\(([\w\.\-_]+)\)|\(\(([\w\.\-_]+)\)\)', re.I)
     elif prefix == 'circus':
         match = _CIRCUS_VAR
     else:
